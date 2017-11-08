a966ecbc2d2a5cefcafba20aeb4742124c297c84
Merlau <joerg.merlau@atos.net>
2017-11-08 13:51:03 +0100

    Postgresql changes.

--- src/main/java/org/openshift/InsultGenerator.java
+++ src/main/java/org/openshift/InsultGenerator.java
@@ -14,6 +14,8 @@ public class InsultGenerator {
 		try {
 			String databaseURL = "jdbc:postgresql://";
 			databaseURL += System.getenv("POSTGRESQL_SERVICE_HOST");
+            databaseURL += ":"
+            databaseURL += System.getenv("POSTGRESQL_SERVICE_PORT");
 			databaseURL += "/" + System.getenv("POSTGRESQL_DATABASE");
 
 			String username = System.getenv("POSTGRESQL_USER");
